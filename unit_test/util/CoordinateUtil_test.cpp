c35076194a1793e73d61535f65210322d711779c
Fahmi Abdulhamid <findfahmi@gmail.com>
2012-01-22 10:47:54 +1300

    Made progress integrating gTest into the project. gTest is still not working properly.

--- unit_test/util/CoordinateUtil_test.cpp
+++ unit_test/util/CoordinateUtil_test.cpp
@@ -1,40 +1,19 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 
-namespace
-{
-    // The fixture for testing class Foo.
-    class FooTest : public ::testing::Test {
-        protected:
-        // You can remove any or all of the following functions if its body
-        // is empty.
+#include <iostream>
+using namespace std;
 
-        FooTest() {
-        // You can do set-up work for each test here.
-        }
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
 
-        virtual ~FooTest() {
-        // You can do clean-up work that doesn't throw exceptions here.
-        }
-
-        // If the constructor and destructor are not enough for setting up
-        // and cleaning up each test, you can define the following methods:
-
-        virtual void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
-        }
-
-        virtual void TearDown() {
-        // Code here will be called immediately after each test (right
-        // before the destructor).
-        }
-
-        // Objects declared here can be used by all tests in the test case for Foo.
-    };
+  return result;
 }
 
-TEST(FooTest, MethodBarDoesAbc)
-{
-    EXPECT_EQ(1, 1);
+TEST(FactorialTest, Zero) {
+  EXPECT_EQ(1, Factorial(0));
+  system("pause");
 }
