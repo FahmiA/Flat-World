9cf345ee7d679568898ae0f701b3fe6bfd762ffc
Fahmi Abdulhamid <findfahmi@gmail.com>
2012-09-05 16:30:18 +1200

    The HUD shows up again!

--- scene/StaticCamera.cpp
+++ scene/StaticCamera.cpp
@@ -3,7 +3,8 @@
 StaticCamera::StaticCamera()
     : Camera(new View())
 {
-    getView()->setViewport(FloatRect(0, 0, 800, 600));
+    getView()->reset(FloatRect(0, 0, 800, 600));
+    getView()->setViewport(FloatRect(0.0f, 0.0f, 1.0f, 1.0f));
 }
 
 StaticCamera::~StaticCamera()
